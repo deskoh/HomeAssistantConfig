b9b535745e2184af6fbcd3e6d6573931c0bf2556
deskoh <deskoh.sg@gmail.com>
2020-06-17 07:48:29 +0800

    Updated scripts

--- python_scripts/room1_mi_switch.py
+++ python_scripts/room1_mi_switch.py
@@ -4,7 +4,8 @@
 # - Single-Click: Turn off/on lights. Cycle lights if already on.
 # - Double-Click: Cycle room scenes
 # - Triple-Click: Turn on all lights
-# - Hold: Set lights to max brightness and reset next scene cycle (so that double click selects 1st scene)
+# - Hold-up: Set lights to max brightness and reset next scene cycle (so that double click selects 1st scene)
+# - Hold-down: Dim down lights
 # Button Pair 2:
 # - Single-Click: Toggle Fan
 # - Double-Click: Cycle fan speed
@@ -47,21 +48,25 @@ elif action == 'button_2_single':
         turn_on(hass, 'light', light_group)
     else:
         # Cycle lights
-        light_group_states = hass.states.get(light_group)
-        light_group_attr = light_group_states.attributes.copy()
-        lights = light_group_attr['entity_id']
+        lights = light_states.attributes.get('entity_id')
         light = lights[0]
+        off_count = 0
         # Get next lights to turn on
-        for i in range(len(lights)):
-            l_states = hass.states.get(lights[i])
+        len = len(lights)
+        for i in range(len):
+            l_states = hass.states.get(lights[(i + len - 1) % len])
             if l_states.state == 'on':
-                light = lights[(i + 1) % len(lights)]
+                light = lights[(i + len) % len]
                 break
-        # Turn off remaining lights
-        for l in lights[0:]:
-            # logger.warn(l + ' compare ' + light + str(light == l))
-            if l == light: turn_on(hass, 'light', l)
-            else: turn_off(hass, 'light', l)
+            else: off_count = off_count + 1
+        # Check if all lights cycled
+        if off_count == len - 1:
+            for l in lights[0:]: turn_on(hass, 'light', l)
+        else:
+            # Turn off remaining lights
+            for l in lights[0:]:
+                if l == light: turn_on(hass, 'light', l)
+                else: turn_off(hass, 'light', l)
 elif action == 'button_1_double' or action == 'button_2_double':
     # Cycle room scenes
     scene = hass.states.get(input_scene).state
@@ -71,9 +76,7 @@ elif action == 'button_1_double' or action == 'button_2_double':
     hass.services.call('input_select', 'select_next', service_data, False)
 elif action == 'button_2_triple':
     # Turn on all lights
-    light_group_states = hass.states.get(light_group)
-    light_group_attr = light_group_states.attributes.copy()
-    lights = light_group_attr['entity_id']
+    lights = light_states.attributes.get('entity_id')
     for l in lights[0:]:
         turn_on(hass, 'light', l)
 elif action == 'button_2_hold':
@@ -82,12 +85,34 @@ elif action == 'button_2_hold':
     hass.services.call('light', 'turn_on', service_data, False)
 
     scene_states = hass.states.get(input_scene)
-    scene_attr = scene_states.attributes.copy()
+    scene_attr_options = hass.states.get(input_scene).attributes.get('options')
     service_data = {
         'entity_id': input_scene,
-        'option': scene_attr['options'][0]
+        'option': scene_attr_options[0]
     }
     hass.services.call('input_select', 'select_option', service_data, False)
+elif action == 'button_1_hold':
+    # Lights dimming control
+    change = -51
+    if light_states.state == 'on':
+        lights = light_states.attributes.get('entity_id')
+        for l in lights[0:]:
+            # To handle case when light is off
+            brightness = 0
+            l_states = hass.states.get(l)
+            if l_states.state == 'on':
+                brightness = l_states.attributes.get('brightness')
+            brightness = brightness + change
+            if brightness > 255:
+                brightness = 255
+            if brightness < 0:
+                brightness = 0
+            logger.warn('Changing brightness for ' + l + ' to ' + str(brightness))
+            service_data = {
+                'entity_id': l,
+                'brightness': brightness
+            }
+            hass.services.call('light', 'turn_on', service_data, False)
 elif action == 'button_3_single' or action == 'button_4_single':
     # Toggle Fan
     toggle_entity(hass, 'switch', 'switch.novita_fan')
