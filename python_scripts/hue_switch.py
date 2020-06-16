3de562ec11c055001269be7672f81648d1de3605
deskoh <deskoh.sg@gmail.com>
2020-06-16 22:22:45 +0800

    Migrated Hue Dimmer Switch to zigbee2mqtt

--- python_scripts/hue_switch.py
+++ python_scripts/hue_switch.py
@@ -12,26 +12,26 @@
 # - Else decrease fan speed, hold to toggle natural mode
 # Button 4:
 # - Turn off lights if on, else announce time.
-# - If hold, toggle upcoming alarm if lights on, else turn off fan.
+# - If hold toggle fan.
 ###############################################################################
 # Script Parameters
-switch_entity = data.get('switch_entity', 'sensor.bedroom_1_switch')
+action = data.get('action', '')
 input_scene = data.get('input_select_scene', 'input_select.room_1_scene')
-input_alarm = data.get('input_alarm', 'input_boolean.room1_wakeup_dismiss')
 light_group = data.get('light_group', 'group.room_1_lights')
 fan_inputs = data.get('fan_inputs')
 speaker = data.get('speaker', 'media_player.room1_speaker')
-
 
 def turn_on(hass, entity, entity_id):
     service_data = {'entity_id': entity_id}
     hass.services.call(entity, 'turn_on', service_data, False)
 
-
 def turn_off(hass, entity, entity_id):
     service_data = {'entity_id': entity_id}
     hass.services.call(entity, 'turn_off', service_data, False)
 
+def toggle_entity(hass, entity, entity_id):
+    service_data = {'entity_id': entity_id}
+    hass.services.call(entity, 'toggle', service_data, False)
 
 def tts(hass, speaker, message):
     service_data = {'entity_id': speaker, 'message': message, 'cache': 'false'}
@@ -45,44 +45,41 @@ else:
     has_fan = False
 
 light_states = hass.states.get(light_group)
-switch_state = hass.states.get(switch_entity).state
 
-if switch_state == '1_click_up':
+if action == 'on-press':
     if light_states.state == 'off':
+        # turn on lights
         turn_on(hass, 'light', light_group)
     elif light_states.state == 'on':
+        # cycle room scenes
         scene = hass.states.get(input_scene).state
         turn_on(hass, 'scene', scene)
 
         service_data = {'entity_id': input_scene}
         hass.services.call('input_select', 'select_next', service_data, False)
-elif switch_state == '1_hold_up':
+elif action == 'on-hold-release':
+    # set all lights to max brightness and reset next scene cycle
     service_data = {'entity_id': light_group, 'brightness': 255}
     hass.services.call('light', 'turn_on', service_data, False)
 
     scene_states = hass.states.get(input_scene)
-    scene_attr = scene_states.attributes.copy()
+    scene_attr_options = hass.states.get(input_scene).attributes.get('options')
     service_data = {
         'entity_id': input_scene,
-        'option': scene_attr['options'][0]
+        'option': scene_attr_options[0]
     }
     hass.services.call('input_select', 'select_option', service_data, False)
-elif switch_state == '4_hold_up':
-    if (light_states.state == 'on'):
-        service_data = {'entity_id': input_alarm}
-        hass.services.call('input_boolean', 'toggle', service_data, False)
-        state = hass.states.get(input_alarm).state
-        message = 'Upcoming alarm is {}!'.format(state)
-        tts(hass, speaker, message)
-    elif has_fan:
-        service_data = {'entity_id': fan}
-        hass.services.call('fan', 'toggle', service_data, False)
-elif switch_state == '2_click_up' or switch_state == '3_click_up' or switch_state == '2_hold_up' or switch_state == '3_hold_up':
+elif action == 'off-hold-release':
+    if has_fan:
+        # Toggle Fan
+        toggle_entity(hass, 'fan', fan)
+        # toggle_entity(hass, 'switch', 'switch.novita_fan')
+elif action == 'up-press' or action == 'down-press' or action == 'up-hold-release' or action == 'down-hold-release':
     if (light_states.state == 'off') and has_fan:
         # Fan control
-        if switch_state == '2_click_up' or switch_state == '3_click_up':
+        if action == 'up-press' or action == 'down-press':
             # Fan speed control
-            change = 10 if switch_state == '2_click_up' else -10
+            change = 10 if action == 'up-press' else -10
             speed = int(float(hass.states.get(fan_speed).state)) + change
             if speed > 100:
                 speed = 100
@@ -92,7 +89,7 @@ elif switch_state == '2_click_up' or switch_state == '3_click_up' or switch_stat
             hass.services.call('input_number', 'set_value',
                                service_data, False)
             tts(hass, speaker, speed)
-        elif switch_state == '2_hold_up':
+        elif action == 'up-hold-release':
             # Fan oscillating control
             swing_angle = hass.states.get(fan_swing).state
             service_data = {'entity_id': fan_swing}
@@ -107,7 +104,7 @@ elif switch_state == '2_click_up' or switch_state == '3_click_up' or switch_stat
                 'input_select', 'select_option', service_data, False)
             service_data = {'entity_id': speaker, 'message': message}
             hass.services.call('tts', 'google_say', service_data, False)
-        elif switch_state == '3_hold_up':
+        elif action == 'down-hold-release':
             # Fan natural mode control
             natural_mode = hass.states.get(fan_natural).state
             if natural_mode == 'on':
@@ -119,28 +116,27 @@ elif switch_state == '2_click_up' or switch_state == '3_click_up' or switch_stat
     else:
         # Lights dimming control
         change = 25 if (
-            switch_state == '2_click_up' or switch_state == '2_hold') else -25
+            action == 'up-press' or action == 'up-hold') else -25
         if light_states.state == 'on':
-            light_group_attr = light_states.attributes.copy()
-            lights = light_group_attr['entity_id']
+            lights = light_states.attributes.get('entity_id')
             for l in lights[0:]:
                 # To handle case when light is off
-                brightness = 25
+                brightness = 0
                 l_states = hass.states.get(l)
                 if l_states.state == 'on':
-                    attr = l_states.attributes.copy()
-                    brightness = attr['brightness'] + change
-                    if brightness > 255:
-                        brightness = 255
-                    if brightness < 0:
-                        brightness = 0
+                    brightness = l_states.attributes.get('brightness')
+                brightness = brightness + change
+                if brightness > 255:
+                    brightness = 255
+                if brightness < 0:
+                    brightness = 0
                 # logger.warn('Changing brightness for ' + l + ' to ' + str(brightness))
                 service_data = {
                     'entity_id': l,
                     'brightness': brightness
                 }
                 hass.services.call('light', 'turn_on', service_data, False)
-elif switch_state == '4_click_up':
+elif action == 'off-press':
     if light_states.state == 'on':
         turn_off(hass, 'light', light_group)
     else:
